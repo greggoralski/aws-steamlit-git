edbb151f1eec1096c405493850f016c5d2ba94b3
greg <greggoralski@gmail.com>
2020-07-28 16:34:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
+
+from pipelines import pipeline
+
+nlp = pipeline("question-generation")
+
+text2 = "Gravity (from Latin gravitas, meaning 'weight'), or gravitation, is a natural phenomenon by which all \
+things with mass or energy—including planets, stars, galaxies, and even light—are brought toward (or gravitate toward) \
+one another. On Earth, gravity gives weight to physical objects, and the Moon's gravity causes the ocean tides. \
+The gravitational attraction of the original gaseous matter present in the Universe caused it to begin coalescing \
+and forming stars and caused the stars to group together into galaxies, so gravity is responsible for many of \
+the large-scale structures in the Universe. Gravity has an infinite range, although its effects become increasingly \
+weaker as objects get further away"
+
+nlp(text2)
 #
 #DATE_COLUMN = 'date/time'
 #DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
